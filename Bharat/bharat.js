0554255681e2c6c3a3416b16a92db1f2f08fd214
Digish Pandya <digish.pandya@gmail.com>
2016-04-10 16:47:08 +0530

    pinch zoom

--- Bharat/bharat.js
+++ Bharat/bharat.js
@@ -161,11 +161,11 @@ function init(){
                         (e.touches[0].x-e.touches[1].x) * (e.touches[0].x-e.touches[1].x) +
                         (e.touches[0].y-e.touches[1].y) * (e.touches[0].y-e.touches[1].y));
                         
-                    //this.onMouseWheel(e,window,dist);
                     
-                    var val = this.controller.Navigation.zooming/1000; 
+                    
+                    var val = this.Navigation.zooming/1000; 
                     //var val = this.config.zooming / 1000,
-                    ans = 1 + dist * val;
+                    var ans = 1 + dist * val;
                     this.canvas.scale(ans, ans);
                 }
             },  
