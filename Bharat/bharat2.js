3399d3610f9c7abbc87626234ca499e7c5703de0
Digish Pandya <digish.pandya@gmail.com>
2016-04-10 14:59:13 +0530

    try new things

--- Bharat/bharat2.js
+++ Bharat/bharat2.js
@@ -15,6 +15,17 @@ var labelType, useGradients, nativeTextSupport, animate;
   animate = !(iStuff || !nativeCanvasSupport);
 })();
 
+$(document).on('pagebeforecreate', '[data-role="page"]', function(){     
+    setTimeout(function(){
+        $.mobile.loading('show');
+    },1);    
+});
+
+$(document).on('pageshow', '[data-role="page"]', function(){  
+    setTimeout(function(){
+        $.mobile.loading('hide');
+    },300);      
+});
 
 var Log = {
   elem: false,
